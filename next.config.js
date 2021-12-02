aefaf2a429084a21299d4f71f75c592d3a2512a5
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-12-01 21:32:13 -0500

    testing

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites(){
+    return [
+      {
+        "source": "/ss/:path*",
+        "destination": "https://scoresaber.com/:path*"
+      },
+      {
+        "source": "/disc/:path*",
+        "destination": "https://discord.com/:path*"
+      }
+    ]
+  }
 }
